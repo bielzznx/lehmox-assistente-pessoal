884f214983f26bfbc4b8705a2350f2a31933afa6
bielzznx <81375810+bielzznx@users.noreply.github.com>
2021-07-19 02:17:10 -0300

    adicionando main.py
    incluir requirement.txt

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 # our main file
 
-import 
+import speech_recognition as sr 
+
+r = sr.recognizer()
+
+with sr.microphone() as source:
+    audio = r.listen(source)
+
+    print(r.recognize_google(audio))
